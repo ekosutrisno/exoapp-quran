8c7577af9892e4d8ab591cf8f76c917600208513
ekosutrisno <ekosutrisno801@gmail.com>
2021-02-20 17:08:37 +0700

    feat: scroll on top and search icon button

--- src/views/QuranMetaList.vue
+++ src/views/QuranMetaList.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="min-h-screen h-screen bg-gray-200 nv-transition">
+   <div ref="pageUp"></div>
   <section class="w-full min-w-min bg-gray-900 py-6 relative">
       <div class="max-w-7xl mx-auto p-4 relative">
          <router-link to="/menu" class="absolute text-gray-300 transition left-10 hover:text-gray-100">
@@ -25,12 +26,7 @@
          </div>
       </div>
   </section>
-  <section class="min-w-min bg-gray-200 pt-10 hero-background">
-     <div class="max-w-7xl mx-auto p-2 mb-4 sticky top-0 z-30">
-         <div class="w-full px-2 flex items-center justify-center">
-            <input v-model="searchInput"  type="text" placeholder="Search surah..."  class="py-3 px-4 rounded w-full max-w-lg mt-6 focus:outline-none ring-2 ring-green-300 ring-opacity-75 focus:ring-opacity-60 the-header"/>
-         </div>
-     </div>
+  <section class="min-w-min pt-20 bg-gray-200 hero-background">
      <div v-if="isProcess" class="flex items-center justify-center nv-transition">
       <Spinner/>
       </div>
@@ -46,11 +42,30 @@
        </div>
      </div>
   </section>
-    <p class="text-center text-sm py-5 text-gray-700">From ExoApp &copy;{{new Date().getFullYear()}} All right reserved</p> 
+  <div v-show="isOn" class="max-w-7xl mx-auto p-2 mb-4 sticky bottom-96 md:bottom-80 z-30 nv-transition">
+      <div class="w-full px-2 flex items-center justify-center">
+         <input v-model="searchInput" ref="formSearch"  type="text" placeholder="Search surah..."  class="py-3 px-4 rounded w-full max-w-lg mt-6 focus:outline-none ring-2 ring-green-300 ring-opacity-75 focus:ring-opacity-60 shadow-2xl"/>
+      </div>
+   </div>
+  <div class="sticky bottom-10 right-4 the-header float-right bg-gray-300 shadow-xl p-2 px-3 rounded-full">
+     <div class="inline-flex space-x-4">
+      <button @click="onSearchInit" class="p-2 rounded-full hover:bg-gray-200 focus:outline-none">
+         <svg class="w-5 text-gray-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+         </svg>
+      </button>
+      <button @click="scrollToPageUp" class="p-2 rounded-full hover:bg-gray-200 focus:outline-none">
+         <svg class="w-5 text-gray-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414l5-5a1 1 0 011.414 0l5 5a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414 0zm0-6a1 1 0 010-1.414l5-5a1 1 0 011.414 0l5 5a1 1 0 01-1.414 1.414L10 5.414 5.707 9.707a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+         </svg>
+      </button>
+     </div>
+  </div>
+    <p class="text-center text-sm text-gray-700">From ExoApp &copy;{{new Date().getFullYear()}} All right reserved</p> 
 </div>
 </template>
 <script>
-import { computed, reactive, toRefs } from 'vue';
+import { computed, nextTick, reactive, ref, toRefs, } from 'vue';
 import { useStore } from 'vuex';
 import QuranMetaCard from '../components/QuranMetaCard.vue';
 import Spinner from '../components/Spinner.vue';
@@ -59,11 +74,15 @@ export default {
    setup(){
 
       const store = useStore();
+      
+      const pageUp = ref(null)
+      const formSearch = ref(null);
 
       const state = reactive({
          isProcess: computed(()=> store.state.surah.isLoading),
          searchInput: '',
          surahs: computed(()=> store.state.surah.surahs),
+         isOn: false
       })
 
       const searching = computed(()=> state.searchInput.trim() !== '');
@@ -81,10 +100,28 @@ export default {
          return searchData;
       
       })
+
+      const scrollToPageUp = () => {
+         pageUp.value.scrollIntoView({behavior: 'smooth'});
+      }
+
+      const onSearchInit = () =>{
+         nextTick(() => {
+            state.isOn = !state.isOn;
+            setTimeout(() => {
+               formSearch.value.focus();
+            }, 1000);
+         })
+      }
+
       return{
          ...toRefs(state),
+         pageUp,
+         formSearch,
          onSearch,
-         searching
+         onSearchInit,
+         searching,
+         scrollToPageUp
       }
    }
 }
